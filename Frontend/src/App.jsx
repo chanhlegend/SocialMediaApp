f355a44eee845cd1fb4f334e06aa91a091644ac0
TranHoangAnh <thathatha2003@gmail.com>
2025-08-19 21:22:05 +0700

    config app

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">
+        Vite + React + Tailwind CSS + MUI
+      </h1>
+      <Button variant="contained">Hello from MUI</Button>
+    </div>
+  )
+}
+
+export default App
+
